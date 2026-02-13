872b7b508374fc0ef827ec79972cc57bbb796461
yitare <yitare@users.noreply.github.com>
2026-02-13 13:42:39 +0800

    Site updated: 2026-02-13 13:42:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/e3c62c27/","posts/b250e9bb/","posts/4a17b156/","posts/e6432aea/"];function toRandomPost(){
+var posts=["posts/e3c62c27/","posts/d1874913/","posts/b250e9bb/","posts/4a17b156/","posts/e6432aea/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
