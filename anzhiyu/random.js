0cd97139b3e54ecad7d9ce903acebb38b021e516
yitare <yitare@users.noreply.github.com>
2026-02-14 13:52:53 +0800

    Site updated: 2026-02-14 13:52:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/e3c62c27/","posts/b250e9bb/","posts/d1874913/","posts/e6432aea/"];function toRandomPost(){
+var posts=["posts/b250e9bb/","posts/d1874913/","posts/e6432aea/","posts/e3c62c27/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
